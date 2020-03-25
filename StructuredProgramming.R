f69c8942eae29bd210a93542f7dfb16d67b18baf
lkshapir <54652737+lkshapir@users.noreply.github.com>
2020-03-25 17:25:36 -0400

    Added HW 7 to index, and created new in-class scripts

--- StructuredProgramming.R
+++ StructuredProgramming.R
@@ -11,3 +11,59 @@
 #summarize_output()
 #graph_results()
 
+
+# Getting input -----------------------------------------------------------
+# SNIPPETS -------------------------
+myfun <- function(x=5) {
+  z <- 5 + runif(1)
+  return(z)
+  }
+myfun() #snippets allow you to autocomplete inputs
+
+#create new snippet: Tools, Global Options, Edit Snippets- second line must be tab  indented
+
+# try using m_bar
+####################################
+#completes with line of hashes
+
+####################################
+
+# try using m_sec
+# get data -------------------------
+# makes section label 
+
+# load libraries -------------------------
+#You can build snippets with R code in them
+
+#try m_head for set up of a new script
+# ----------------------------------------
+# script description
+
+# ----------------------------------------
+# Tells what this new script is about
+# 24 Mar 2020
+# LKS
+# ----------------------------------------
+#
+
+# try out cool new function snippet
+# ----------------------------------------
+# FUNCTION trial_funct
+# description: one line explanation here
+# inputs: just x = 5 for now
+# outputs: real number
+##########################################
+trial_funct <- function(x=5) {
+
+# function body
+
+return("Checking...trial_funct")
+
+} # end of trial_funct
+#-----------------------------------------
+trial_funct()
+
+
+
+
+
